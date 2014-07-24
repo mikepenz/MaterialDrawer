6968ade9b4316c28032cbe629ff1251290c7d534
Mike Penz <mikepenz@gmail.com>
2014-07-24 14:52:28 +0200

    * introduce HackyDrawerLayout to fix issues with zoomingImageViews

---
+++ library/src/main/java/com/tundem/holokitkatdrawer/view/HackyDrawerLayout.java
@@ -0,0 +1,32 @@
+package com.tundem.holokitkatdrawer.view;
+
+import android.content.Context;
+import android.support.v4.widget.DrawerLayout;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class HackyDrawerLayout extends DrawerLayout {
+
+
+    public HackyDrawerLayout(Context context) {
+        super(context);
+    }
+
+    public HackyDrawerLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public HackyDrawerLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        try {
+            return super.onInterceptTouchEvent(ev);
+        } catch (Throwable t) {
+            t.printStackTrace();
+            return false;
+        }
+    }
+}
