f693c7253783a069eb2731f7e7cdf618a2a9c74a
Mike Penz <mikepenz@gmail.com>
2015-02-19 20:53:52 +0100

    * refractor styles once again. Creating a Translucent theme and a non translucent theme

---
+++ app/src/main/java/com/mikepenz/materialdrawer/app/SimpleNonTranslucentDrawerActivity.java
@@ -0,0 +1,76 @@
+package com.mikepenz.materialdrawer.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
+
+import com.mikepenz.iconics.typeface.FontAwesome;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SectionDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.Nameable;
+
+public class SimpleNonTranslucentDrawerActivity extends ActionBarActivity {
+
+    private Drawer.Result result = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //supportRequestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample);
+
+        // Handle Toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        result = new Drawer()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withActionBarDrawerToggle(true)
+                .addDrawerItems(
+                        new PrimaryDrawerItem().withName(R.string.drawer_item_action_bar).withIcon(FontAwesome.Icon.faw_home).withIdentifier(1),
+                        new PrimaryDrawerItem().withName(R.string.drawer_item_multi_drawer).withIcon(FontAwesome.Icon.faw_gamepad).withIdentifier(2),
+                        new PrimaryDrawerItem().withName(R.string.drawer_item_custom).withIcon(FontAwesome.Icon.faw_eye),
+                        new SectionDrawerItem().withName(R.string.drawer_item_section_header),
+                        new SecondaryDrawerItem().withName(R.string.drawer_item_settings).withIcon(FontAwesome.Icon.faw_cog),
+                        new SecondaryDrawerItem().withName(R.string.drawer_item_help).withIcon(FontAwesome.Icon.faw_question).setEnabled(false),
+                        new SecondaryDrawerItem().withName(R.string.drawer_item_open_source).withIcon(FontAwesome.Icon.faw_github),
+                        new SecondaryDrawerItem().withName(R.string.drawer_item_contact).withIcon(FontAwesome.Icon.faw_bullhorn)
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id, IDrawerItem drawerItem) {
+                        if (drawerItem instanceof Nameable) {
+                            Toast.makeText(SimpleNonTranslucentDrawerActivity.this, SimpleNonTranslucentDrawerActivity.this.getString(((Nameable) drawerItem).getNameRes()), Toast.LENGTH_SHORT).show();
+                        }
+
+                        if (drawerItem.getIdentifier() == 1) {
+                            Intent intent = new Intent(SimpleNonTranslucentDrawerActivity.this, ActionBarDrawerActivity.class);
+                            SimpleNonTranslucentDrawerActivity.this.startActivity(intent);
+                        } else if (drawerItem.getIdentifier() == 2) {
+                            Intent intent = new Intent(SimpleNonTranslucentDrawerActivity.this, MultiDrawerActivity.class);
+                            SimpleNonTranslucentDrawerActivity.this.startActivity(intent);
+                        }
+                    }
+                })
+                .withSelectedItem(2)
+                .withSavedInstance(savedInstanceState)
+                .build();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState = result.saveInstanceState(outState);
+         super.onSaveInstanceState(outState);
+    }
+}
