c51fbaa4551efa1eca6ae72b9dd9619ed6a85822
Mike Penz <mikepenz@gmail.com>
2015-02-25 21:58:54 +0100

    * fix small bug within the drawer

--- library/src/main/java/com/mikepenz/materialdrawer/Drawer.java
+++ library/src/main/java/com/mikepenz/materialdrawer/Drawer.java
@@ -867,7 +867,7 @@ public class Drawer {
                 return mDrawerItems.get(position - mHeaderOffset);
             }
         } else {
-            if (mDrawerItems != null && mDrawerItems.size() > mCurrentSelection && mCurrentSelection > -1) {
+            if (mDrawerItems != null && mDrawerItems.size() > position && position > -1) {
                 return mDrawerItems.get(position);
             }
         }
@@ -887,7 +887,7 @@ public class Drawer {
                 return true;
             }
         } else {
-            if (mDrawerItems != null && mDrawerItems.size() > mCurrentSelection && mCurrentSelection > -1) {
+            if (mDrawerItems != null && mDrawerItems.size() > position && position > -1) {
                 return true;
             }
         }
