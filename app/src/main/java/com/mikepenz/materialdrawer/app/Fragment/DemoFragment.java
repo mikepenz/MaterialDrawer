8944955d4f2fdf73f36c36474f0f5106847999e2
Mike Penz <mikepenz@gmail.com>
2015-03-14 15:43:51 +0100

    * add new SimpleFragmentDrawerActivity sample activity

---
+++ app/src/main/java/com/mikepenz/materialdrawer/app/Fragment/DemoFragment.java
@@ -0,0 +1,40 @@
+package com.mikepenz.materialdrawer.app.Fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mikepenz.materialdrawer.app.R;
+
+
+/**
+ * A simple {@link android.support.v4.app.Fragment} subclass.
+ */
+public class DemoFragment extends Fragment {
+    private static final String KEY_TITLE = "title";
+
+    public DemoFragment() {
+        // Required empty public constructor
+    }
+
+    public static DemoFragment newInstance(String title) {
+        DemoFragment f = new DemoFragment();
+
+        Bundle args = new Bundle();
+
+        args.putString(KEY_TITLE, title);
+        f.setArguments(args);
+
+        return (f);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        // don't look at this layout it's just a listView to show how to handle the keyboard
+        return inflater.inflate(R.layout.fragment_sample, container, false);
+    }
+}
---
+++ app/src/main/java/com/mikepenz/materialdrawer/app/SimpleFragmentDrawerActivity.java
@@ -0,0 +1,99 @@
+package com.mikepenz.materialdrawer.app;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+
+import com.mikepenz.iconics.typeface.FontAwesome;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.app.Fragment.DemoFragment;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SectionDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.Nameable;
+import com.mikepenz.materialdrawer.util.KeyboardUtil;
+
+import java.util.ArrayList;
+
+public class SimpleFragmentDrawerActivity extends ActionBarActivity {
+
+    ArrayList<Fragment> fragments = new ArrayList<Fragment>();
+
+    //save our header or result
+    private Drawer.Result result = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample_fragment);
+
+        // Handle Toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Create the drawer
+        result = new Drawer()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .addDrawerItems(
+                        new PrimaryDrawerItem().withName(R.string.drawer_item_home).withIcon(FontAwesome.Icon.faw_home),
+                        new PrimaryDrawerItem().withName(R.string.drawer_item_free_play).withIcon(FontAwesome.Icon.faw_gamepad),
+                        new PrimaryDrawerItem().withName(R.string.drawer_item_custom).withIcon(FontAwesome.Icon.faw_eye),
+                        new SectionDrawerItem().withName(R.string.drawer_item_section_header),
+                        new SecondaryDrawerItem().withName(R.string.drawer_item_settings).withIcon(FontAwesome.Icon.faw_cog),
+                        new SecondaryDrawerItem().withName(R.string.drawer_item_help).withIcon(FontAwesome.Icon.faw_question).setEnabled(false),
+                        new SecondaryDrawerItem().withName(R.string.drawer_item_open_source).withIcon(FontAwesome.Icon.faw_github),
+                        new SecondaryDrawerItem().withName(R.string.drawer_item_contact).withIcon(FontAwesome.Icon.faw_bullhorn)
+                ) // add the items we want to use with our Drawer
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id, IDrawerItem drawerItem) {
+                        if (drawerItem != null && drawerItem instanceof Nameable) {
+                            getSupportActionBar().setTitle(((Nameable) drawerItem).getNameRes());
+                            //ignore the DemoFragment and it's layout it's just to showcase the handle with an keyboard
+                            Fragment f = DemoFragment.newInstance(getResources().getString(((Nameable) drawerItem).getNameRes()));
+                            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, f).commit();
+                        }
+                    }
+                })
+                .withOnDrawerListener(new Drawer.OnDrawerListener() {
+                    @Override
+                    public void onDrawerOpened(View drawerView) {
+                        KeyboardUtil.hideKeyboard(SimpleFragmentDrawerActivity.this);
+                    }
+
+                    @Override
+                    public void onDrawerClosed(View drawerView) {
+
+                    }
+                })
+                .withFireOnInitialOnClick(true)
+                .withSavedInstance(savedInstanceState)
+                .build();
+
+        //react on the keyboard
+        new KeyboardUtil(this, findViewById(R.id.fragment_container));
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        //add the values which need to be saved from the drawer to the bundle
+        outState = result.saveInstanceState(outState);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onBackPressed() {
+        //handle the back press :D close the drawer first and if the drawer is closed close the activity
+        if (result != null && result.isDrawerOpen()) {
+            result.closeDrawer();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+}
