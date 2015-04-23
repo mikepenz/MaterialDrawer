9409dd856fe89220e5562fdd1e13bf893216e9cc
Mike Penz <mikepenz@gmail.com>
2015-04-23 20:59:19 +0200

    * add info how to get a full transparent statusbar

--- app/src/main/java/com/mikepenz/materialdrawer/app/FullscreenDrawerActivity.java
+++ app/src/main/java/com/mikepenz/materialdrawer/app/FullscreenDrawerActivity.java
@@ -62,7 +62,37 @@ public class FullscreenDrawerActivity extends AppCompatActivity {
         //set the back arrow in the toolbar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(false);
+
+
+        /*
+        //USE THIS CODE TO GET A FULL TRANSPARENT STATUS BAR
+        //YOU HAVE TO UNCOMMENT THE setWindowFlag too.
+        if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
+            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true);
+        }
+        if (Build.VERSION.SDK_INT >= 19) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+        if (Build.VERSION.SDK_INT >= 21) {
+            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+        */
     }
+
+    /*
+
+    public static void setWindowFlag(Activity activity, final int bits, boolean on) {
+        Window win = activity.getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
+    }
+    */
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
