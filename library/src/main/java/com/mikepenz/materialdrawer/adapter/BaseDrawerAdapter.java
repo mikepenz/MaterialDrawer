14e2a1a922436108e066ace7c409abf56c2f4cfc
Mike Penz <mikepenz@gmail.com>
2015-07-18 19:18:06 +0200

    * fix index out of bounds exception

--- library/src/main/java/com/mikepenz/materialdrawer/adapter/BaseDrawerAdapter.java
+++ library/src/main/java/com/mikepenz/materialdrawer/adapter/BaseDrawerAdapter.java
@@ -246,13 +246,13 @@ public abstract class BaseDrawerAdapter extends RecyclerView.Adapter<RecyclerVie
     }
 
     public IDrawerItem getItem(int position) {
-        if (position < 0 || position > getItemCount()) {
+        if (position < 0 || position >= getItemCount()) {
             return null;
         }
 
         if (position < getHeaderItemCount()) {
             return mHeaderDrawerItems.get(position);
-        } else if (position < getHeaderItemCount() + getDrawerItemCount()) {
+        } else if (position < (getHeaderItemCount() + getDrawerItemCount())) {
             return mDrawerItems.get(position - getHeaderItemCount());
         } else {
             return mFooterDrawerItems.get(position - getHeaderItemCount() - getDrawerItemCount());
