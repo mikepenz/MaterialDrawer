dfb6e9e482436f2a5899cc8ea67dd4d261853b7a
Bernat Borrás Paronella <bernat.borras@worldline.com>
2015-09-05 20:06:21 +0200

    Created BaseSecondaryDrawerItem in order to automatically get the correct color for secondary items

--- library/src/main/java/com/mikepenz/materialdrawer/model/SecondarySwitchDrawerItem.java
+++ library/src/main/java/com/mikepenz/materialdrawer/model/SecondarySwitchDrawerItem.java
@@ -7,11 +7,11 @@ import android.support.annotation.ColorRes;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.StringRes;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SwitchCompat;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.ToggleButton;
 
 import com.mikepenz.materialdrawer.R;
 import com.mikepenz.materialdrawer.holder.ColorHolder;
@@ -25,48 +25,52 @@ import com.mikepenz.materialize.util.UIUtils;
 /**
  * Created by mikepenz on 03.02.15.
  */
-public class SecondaryToggleDrawerItem extends BaseSecondaryDrawerItem<SecondaryToggleDrawerItem> {
+public class SecondarySwitchDrawerItem extends BaseSecondaryDrawerItem<SecondarySwitchDrawerItem> {
     private StringHolder description;
     private ColorHolder descriptionTextColor;
 
-    private boolean toggleEnabled = true;
+    private boolean switchEnabled = true;
 
     private boolean checked = false;
     private OnCheckedChangeListener onCheckedChangeListener = null;
 
-    public SecondaryToggleDrawerItem withDescription(String description) {
+    public SecondarySwitchDrawerItem withDescription(String description) {
         this.description = new StringHolder(description);
         return this;
     }
 
-    public SecondaryToggleDrawerItem withDescription(@StringRes int descriptionRes) {
+    public SecondarySwitchDrawerItem withDescription(@StringRes int descriptionRes) {
         this.description = new StringHolder(descriptionRes);
         return this;
     }
 
-    public SecondaryToggleDrawerItem withDescriptionTextColor(@ColorInt int color) {
+    public SecondarySwitchDrawerItem withDescriptionTextColor(@ColorInt int color) {
         this.descriptionTextColor = ColorHolder.fromColor(color);
         return this;
     }
 
-    public SecondaryToggleDrawerItem withDescriptionTextColorRes(@ColorRes int colorRes) {
+    public SecondarySwitchDrawerItem withDescriptionTextColorRes(@ColorRes int colorRes) {
         this.descriptionTextColor = ColorHolder.fromColorRes(colorRes);
         return this;
     }
 
-    public SecondaryToggleDrawerItem withChecked(boolean checked) {
+    public SecondarySwitchDrawerItem withChecked(boolean checked) {
         this.checked = checked;
         return this;
     }
 
-    public SecondaryToggleDrawerItem withToggleEnabled(boolean toggleEnabled) {
-        this.toggleEnabled = toggleEnabled;
+    public SecondarySwitchDrawerItem withSwitchEnabled(boolean switchEnabled) {
+        this.switchEnabled = switchEnabled;
         return this;
     }
 
-    public SecondaryToggleDrawerItem withOnCheckedChangeListener(OnCheckedChangeListener onCheckedChangeListener) {
+    public SecondarySwitchDrawerItem withOnCheckedChangeListener(OnCheckedChangeListener onCheckedChangeListener) {
         this.onCheckedChangeListener = onCheckedChangeListener;
         return this;
+    }
+
+    public SecondarySwitchDrawerItem withCheckable(boolean checkable) {
+        return withSelectable(checkable);
     }
 
     public StringHolder getDescription() {
@@ -81,35 +85,23 @@ public class SecondaryToggleDrawerItem extends BaseSecondaryDrawerItem<Secondary
         return checked;
     }
 
-    public void setChecked(boolean checked) {
-        this.checked = checked;
-    }
-
-    public boolean isToggleEnabled() {
-        return toggleEnabled;
-    }
-
-    public void setToggleEnabled(boolean toggleEnabled) {
-        this.toggleEnabled = toggleEnabled;
+    public boolean isSwitchEnabled() {
+        return switchEnabled;
     }
 
     public OnCheckedChangeListener getOnCheckedChangeListener() {
         return onCheckedChangeListener;
     }
 
-    public void setOnCheckedChangeListener(OnCheckedChangeListener onCheckedChangeListener) {
-        this.onCheckedChangeListener = onCheckedChangeListener;
-    }
-
     @Override
     public String getType() {
-        return "SECONDARY_TOGGLE_ITEM";
+        return "SECONDARY_SWITCH_ITEM";
     }
 
     @Override
     @LayoutRes
     public int getLayoutRes() {
-        return R.layout.material_secondary_drawer_item_toggle;
+        return R.layout.material_secondary_drawer_item_switch;
     }
 
     @Override
@@ -142,21 +134,20 @@ public class SecondaryToggleDrawerItem extends BaseSecondaryDrawerItem<Secondary
         //set the text for the description or hide
         StringHolder.applyToOrHide(this.getDescription(), viewHolder.description);
 
-
         if (!isSelectable()) {
             viewHolder.view.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if (toggleEnabled) {
-                        viewHolder.toggle.setChecked(!viewHolder.toggle.isChecked());
+                    if (switchEnabled) {
+                        viewHolder.switchView.setChecked(!viewHolder.switchView.isChecked());
                     }
                 }
             });
         }
 
-        viewHolder.toggle.setChecked(checked);
-        viewHolder.toggle.setOnCheckedChangeListener(checkedChangeListener);
-        viewHolder.toggle.setEnabled(toggleEnabled);
+        viewHolder.switchView.setChecked(checked);
+        viewHolder.switchView.setOnCheckedChangeListener(checkedChangeListener);
+        viewHolder.switchView.setEnabled(switchEnabled);
 
         //set the colors for textViews
         viewHolder.name.setTextColor(getTextColorStateList(color, selectedTextColor));
@@ -197,7 +188,7 @@ public class SecondaryToggleDrawerItem extends BaseSecondaryDrawerItem<Secondary
         private ImageView icon;
         private TextView name;
         private TextView description;
-        private ToggleButton toggle;
+        private SwitchCompat switchView;
 
         private ViewHolder(View view) {
             super(view);
@@ -205,7 +196,7 @@ public class SecondaryToggleDrawerItem extends BaseSecondaryDrawerItem<Secondary
             this.icon = (ImageView) view.findViewById(R.id.material_drawer_icon);
             this.name = (TextView) view.findViewById(R.id.material_drawer_name);
             this.description = (TextView) view.findViewById(R.id.material_drawer_description);
-            this.toggle = (ToggleButton) view.findViewById(R.id.material_drawer_toggle);
+            this.switchView = (SwitchCompat) view.findViewById(R.id.material_drawer_switch);
         }
     }
 
@@ -215,7 +206,7 @@ public class SecondaryToggleDrawerItem extends BaseSecondaryDrawerItem<Secondary
             checked = isChecked;
 
             if (getOnCheckedChangeListener() != null) {
-                getOnCheckedChangeListener().onCheckedChanged(SecondaryToggleDrawerItem.this, buttonView, isChecked);
+                getOnCheckedChangeListener().onCheckedChanged(SecondarySwitchDrawerItem.this, buttonView, isChecked);
             }
         }
     };
