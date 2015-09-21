c54fcaa2a8b3ce9b97f281365a0a925accc66df9
Mike Penz <mikepenz@gmail.com>
2015-09-21 19:50:45 +0200

    * change DrawerImageLoader sample implementation to the new AbstractDrawerImageLoader * showcase how to define different placeholders for different tags

--- app/src/main/java/com/mikepenz/materialdrawer/app/CustomApplication.java
+++ app/src/main/java/com/mikepenz/materialdrawer/app/CustomApplication.java
@@ -7,7 +7,10 @@ import android.net.Uri;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.mikepenz.iconics.IconicsDrawable;
+import com.mikepenz.materialdrawer.util.AbstractDrawerImageLoader;
 import com.mikepenz.materialdrawer.util.DrawerImageLoader;
+import com.mikepenz.materialdrawer.util.DrawerUIUtils;
 
 /**
  * Created by mikepenz on 27.03.15.
@@ -39,7 +42,7 @@ public class CustomApplication extends Application {
         */
 
         //initialize and create the image loader logic
-        DrawerImageLoader.init(new DrawerImageLoader.IDrawerImageLoader() {
+        DrawerImageLoader.init(new AbstractDrawerImageLoader() {
             @Override
             public void set(ImageView imageView, Uri uri, Drawable placeholder) {
                 Glide.with(imageView.getContext()).load(uri).placeholder(placeholder).into(imageView);
@@ -51,8 +54,22 @@ public class CustomApplication extends Application {
             }
 
             @Override
-            public Drawable placeholder(Context ctx) {
-                return null;
+            public Drawable placeholder(Context ctx, String tag) {
+                //define different placeholders for different imageView targets
+                //default tags are accessible via the DrawerImageLoader.Tags
+                //custom ones can be checked via string. see the CustomUrlBasePrimaryDrawerItem LINE 111
+                if (DrawerImageLoader.Tags.PROFILE.name().equals(tag)) {
+                    return DrawerUIUtils.getPlaceHolder(ctx);
+                } else if (DrawerImageLoader.Tags.ACCOUNT_HEADER.name().equals(tag)) {
+                    return new IconicsDrawable(ctx).iconText(" ").backgroundColorRes(com.mikepenz.materialdrawer.R.color.primary).sizeDp(56);
+                } else if ("customUrlItem".equals(tag)) {
+                    return new IconicsDrawable(ctx).iconText(" ").backgroundColorRes(R.color.md_red_500).sizeDp(56);
+                }
+
+                //we use the default one for
+                //DrawerImageLoader.Tags.PROFILE_DRAWER_ITEM.name()
+
+                return super.placeholder(ctx, tag);
             }
         });
     }
