1f33e3ece83308267d675c3b645269f521157032
Mike Penz <mikepenz@gmail.com>
2015-09-21 19:53:41 +0200

    * change to .actionBar() away from the deprecated .actionBarSize()

--- app/src/main/java/com/mikepenz/materialdrawer/app/DrawerActivity.java
+++ app/src/main/java/com/mikepenz/materialdrawer/app/DrawerActivity.java
@@ -78,7 +78,7 @@ public class DrawerActivity extends AppCompatActivity {
                         profile5,
                         profile6,
                         //don't ask but google uses 14dp for the add account icon in gmail but 20dp for the normal icons (like manage account)
-                        new ProfileSettingDrawerItem().withName("Add Account").withDescription("Add new GitHub Account").withIcon(new IconicsDrawable(this, GoogleMaterial.Icon.gmd_add).actionBarSize().paddingDp(5).colorRes(R.color.material_drawer_primary_text)).withIdentifier(PROFILE_SETTING),
+                        new ProfileSettingDrawerItem().withName("Add Account").withDescription("Add new GitHub Account").withIcon(new IconicsDrawable(this, GoogleMaterial.Icon.gmd_add).actionBar().paddingDp(5).colorRes(R.color.material_drawer_primary_text)).withIdentifier(PROFILE_SETTING),
                         new ProfileSettingDrawerItem().withName("Manage Account").withIcon(GoogleMaterial.Icon.gmd_settings)
                 )
                 .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
