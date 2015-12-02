acf903a9b4a5bf15200aa23d20e98c0e3265201c
Mike Penz <mikepenz@gmail.com>
2015-12-02 21:59:14 +0100

    * fix if statement from the new handleAllUris feature

--- library/src/main/java/com/mikepenz/materialdrawer/util/DrawerImageLoader.java
+++ library/src/main/java/com/mikepenz/materialdrawer/util/DrawerImageLoader.java
@@ -50,7 +50,7 @@ public class DrawerImageLoader {
      */
     public boolean setImage(ImageView imageView, Uri uri, String tag) {
         //if we do not handle all uris and are not http or https we keep the original behavior
-        if (mHandleAllUris || "http".equals(uri.getScheme()) || !"https".equals(uri.getScheme())) {
+        if (mHandleAllUris || "http".equals(uri.getScheme()) || "https".equals(uri.getScheme())) {
             if (imageLoader != null) {
                 Drawable placeHolder = imageLoader.placeholder(imageView.getContext(), tag);
                 imageLoader.set(imageView, uri, placeHolder);
