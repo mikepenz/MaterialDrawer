f6a5938e3da0b3ee0675211bcfe4fd0fb8507d5e
Mike Penz <mikepenz@gmail.com>
2016-02-23 21:34:22 +0100

    * add note for API 10 inside the ExpandableDrawerItem

--- library/src/main/java/com/mikepenz/materialdrawer/model/ExpandableDrawerItem.java
+++ library/src/main/java/com/mikepenz/materialdrawer/model/ExpandableDrawerItem.java
@@ -16,6 +16,7 @@ import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 /**
  * Created by mikepenz on 03.02.15.
+ * NOTE: The arrow will just animate (and rotate) on APIs >= 11 as the ViewCompat will skip this on API 10
  */
 public class ExpandableDrawerItem extends BasePrimaryDrawerItem<ExpandableDrawerItem, ExpandableDrawerItem.ViewHolder> {
 
