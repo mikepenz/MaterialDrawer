be730539e86b3459919d1a964adda177c019ce82
Mike Penz <mikepenz@gmail.com>
2016-04-13 19:34:10 +0200

    * remove the BaseSecondaryDrawerItem and extend fromt he Primary items for the Secondary items as they are the same just with a different layout and height  * FIX #1176

--- library/src/main/java/com/mikepenz/materialdrawer/model/SecondaryDrawerItem.java
+++ library/src/main/java/com/mikepenz/materialdrawer/model/SecondaryDrawerItem.java
@@ -2,54 +2,14 @@ package com.mikepenz.materialdrawer.model;
 
 import android.content.Context;
 import android.support.annotation.LayoutRes;
-import android.support.annotation.StringRes;
-import android.view.View;
-import android.widget.TextView;
 
-import com.mikepenz.fastadapter.utils.ViewHolderFactory;
 import com.mikepenz.materialdrawer.R;
-import com.mikepenz.materialdrawer.holder.BadgeStyle;
-import com.mikepenz.materialdrawer.holder.StringHolder;
-import com.mikepenz.materialdrawer.model.interfaces.ColorfulBadgeable;
+import com.mikepenz.materialdrawer.holder.ColorHolder;
 
 /**
  * Created by mikepenz on 03.02.15.
  */
-public class SecondaryDrawerItem extends BaseSecondaryDrawerItem<SecondaryDrawerItem, SecondaryDrawerItem.ViewHolder> implements ColorfulBadgeable<SecondaryDrawerItem> {
-    protected StringHolder mBadge;
-    protected BadgeStyle mBadgeStyle = new BadgeStyle();
-
-    @Override
-    public SecondaryDrawerItem withBadge(StringHolder badge) {
-        this.mBadge = badge;
-        return this;
-    }
-
-    @Override
-    public SecondaryDrawerItem withBadge(String badge) {
-        this.mBadge = new StringHolder(badge);
-        return this;
-    }
-
-    @Override
-    public SecondaryDrawerItem withBadge(@StringRes int badgeRes) {
-        this.mBadge = new StringHolder(badgeRes);
-        return this;
-    }
-
-    @Override
-    public SecondaryDrawerItem withBadgeStyle(BadgeStyle badgeStyle) {
-        this.mBadgeStyle = badgeStyle;
-        return this;
-    }
-
-    public StringHolder getBadge() {
-        return mBadge;
-    }
-
-    public BadgeStyle getBadgeStyle() {
-        return mBadgeStyle;
-    }
+public class SecondaryDrawerItem extends PrimaryDrawerItem {
 
     @Override
     public int getType() {
@@ -62,51 +22,21 @@ public class SecondaryDrawerItem extends BaseSecondaryDrawerItem<SecondaryDrawer
         return R.layout.material_drawer_item_secondary;
     }
 
+    /**
+     * helper method to decide for the correct color
+     * OVERWRITE to get the correct secondary color
+     *
+     * @param ctx
+     * @return
+     */
     @Override
-    public void bindView(ViewHolder viewHolder) {
-        Context ctx = viewHolder.itemView.getContext();
-
-        //bind the basic view parts
-        bindViewHelper(viewHolder);
-
-        //set the text for the badge or hide
-        boolean badgeVisible = StringHolder.applyToOrHide(mBadge, viewHolder.badge);
-        //style the badge if it is visible
-        if (badgeVisible) {
-            mBadgeStyle.style(viewHolder.badge, getTextColorStateList(getColor(ctx), getSelectedTextColor(ctx)));
-            viewHolder.badgeContainer.setVisibility(View.VISIBLE);
+    protected int getColor(Context ctx) {
+        int color;
+        if (this.isEnabled()) {
+            color = ColorHolder.color(getTextColor(), ctx, R.attr.material_drawer_secondary_text, R.color.material_drawer_secondary_text);
         } else {
-            viewHolder.badgeContainer.setVisibility(View.GONE);
+            color = ColorHolder.color(getDisabledTextColor(), ctx, R.attr.material_drawer_hint_text, R.color.material_drawer_hint_text);
         }
-
-        //define the typeface for our textViews
-        if (getTypeface() != null) {
-            viewHolder.badge.setTypeface(getTypeface());
-        }
-
-        //call the onPostBindView method to trigger post bind view actions (like the listener to modify the item if required)
-        onPostBindView(this, viewHolder.itemView);
-    }
-
-    @Override
-    public ViewHolderFactory<ViewHolder> getFactory() {
-        return new ItemFactory();
-    }
-
-    public static class ItemFactory implements ViewHolderFactory<ViewHolder> {
-        public ViewHolder create(View v) {
-            return new ViewHolder(v);
-        }
-    }
-
-    protected static class ViewHolder extends BaseViewHolder {
-        private View badgeContainer;
-        private TextView badge;
-
-        private ViewHolder(View view) {
-            super(view);
-            this.badgeContainer = view.findViewById(R.id.material_drawer_badge_container);
-            this.badge = (TextView) view.findViewById(R.id.material_drawer_badge);
-        }
+        return color;
     }
 }
