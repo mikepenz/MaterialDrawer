394e4be6f4aeb00bafb248b1bb23d833b247575e
Mike Penz <mikepenz@gmail.com>
2019-12-28 15:52:38 +0100

    * add iconics module to release script

--- release.sh
+++ release.sh
@@ -6,6 +6,7 @@ if [ "$1" = "release" ];
 then
     ./gradlew library:bintrayUpload -Plibrary_only
     ./gradlew library-nav:bintrayUpload -x test -x lint -Plibrary_nav_only
+    ./gradlew library-iconics:bintrayUpload -x test -x lint -Plibrary_iconics_only
 else
     //TODO
 fi
