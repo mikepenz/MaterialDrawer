d9d8e6abe17923b66e1e7dc0564cafa5795ad3cb
Mike Penz <mikepenz@gmail.com>
2020-06-20 17:53:22 +0200

    - add swipeRefreshLayout to CollapsingToolbarActivity.kt sample app

--- app/src/main/java/com/mikepenz/materialdrawer/app/CollapsingToolbarActivity.kt
+++ app/src/main/java/com/mikepenz/materialdrawer/app/CollapsingToolbarActivity.kt
@@ -10,6 +10,7 @@ import com.mikepenz.iconics.typeface.library.fontawesome.FontAwesome
 import com.mikepenz.iconics.typeface.library.googlematerial.GoogleMaterial
 import com.mikepenz.iconics.utils.actionBar
 import com.mikepenz.iconics.utils.colorInt
+import com.mikepenz.materialdrawer.app.utils.convertDpToPixel
 import com.mikepenz.materialdrawer.holder.ImageHolder
 import com.mikepenz.materialdrawer.iconics.withIcon
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
@@ -18,7 +19,9 @@ import com.mikepenz.materialdrawer.model.SectionDrawerItem
 import com.mikepenz.materialdrawer.model.interfaces.withEnabled
 import com.mikepenz.materialdrawer.model.interfaces.withIdentifier
 import com.mikepenz.materialdrawer.model.interfaces.withName
+import com.mikepenz.materialdrawer.util.removeAllItems
 import com.mikepenz.materialdrawer.widget.AccountHeaderView
+import com.mikepenz.materialdrawer.widget.MaterialDrawerSliderView
 import kotlinx.android.synthetic.main.activity_sample_collapsing_toolbar.*
 
 class CollapsingToolbarActivity : AppCompatActivity() {
@@ -32,6 +35,14 @@ class CollapsingToolbarActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         collapsingToolbar.title = getString(R.string.drawer_item_collapsing_toolbar_drawer)
 
+        material_drawer_swipe_refresh.setProgressViewOffset(true, 0, convertDpToPixel(48f, this).toInt())
+        material_drawer_swipe_refresh.setOnRefreshListener {
+            material_drawer_swipe_refresh.postDelayed({
+                slider.setDrawerItems()
+                slider.setSelection(5, false)
+                material_drawer_swipe_refresh.isRefreshing = false
+            }, 3000)
+        }
 
         // Create the AccountHeader
         headerView = AccountHeaderView(this).apply {
@@ -41,21 +52,27 @@ class CollapsingToolbarActivity : AppCompatActivity() {
         }
 
         slider.apply {
-            itemAdapter.add(
-                    PrimaryDrawerItem().withName(R.string.drawer_item_home).withIcon(FontAwesome.Icon.faw_home).withIdentifier(1),
-                    PrimaryDrawerItem().withName(R.string.drawer_item_free_play).withIcon(FontAwesome.Icon.faw_gamepad),
-                    PrimaryDrawerItem().withName(R.string.drawer_item_custom).withIcon(FontAwesome.Icon.faw_eye).withIdentifier(5),
-                    SectionDrawerItem().withName(R.string.drawer_item_section_header),
-                    SecondaryDrawerItem().withName(R.string.drawer_item_settings).withIcon(FontAwesome.Icon.faw_cog),
-                    SecondaryDrawerItem().withName(R.string.drawer_item_help).withIcon(FontAwesome.Icon.faw_question).withEnabled(false),
-                    SecondaryDrawerItem().withName(R.string.drawer_item_open_source).withIcon(FontAwesome.Icon.faw_github),
-                    SecondaryDrawerItem().withName(R.string.drawer_item_contact).withIcon(FontAwesome.Icon.faw_bullhorn)
-            )
+            setDrawerItems()
+            setSelection(1, false)
             setSavedInstance(savedInstanceState)
         }
 
         fillFab()
         loadBackdrop()
+    }
+
+    private fun MaterialDrawerSliderView.setDrawerItems() {
+        removeAllItems()
+        itemAdapter.add(
+                PrimaryDrawerItem().withName(R.string.drawer_item_home).withIcon(FontAwesome.Icon.faw_home).withIdentifier(1),
+                PrimaryDrawerItem().withName(R.string.drawer_item_free_play).withIcon(FontAwesome.Icon.faw_gamepad),
+                PrimaryDrawerItem().withName(R.string.drawer_item_custom).withIcon(FontAwesome.Icon.faw_eye).withIdentifier(5),
+                SectionDrawerItem().withName(R.string.drawer_item_section_header),
+                SecondaryDrawerItem().withName(R.string.drawer_item_settings).withIcon(FontAwesome.Icon.faw_cog),
+                SecondaryDrawerItem().withName(R.string.drawer_item_help).withIcon(FontAwesome.Icon.faw_question).withEnabled(false),
+                SecondaryDrawerItem().withName(R.string.drawer_item_open_source).withIcon(FontAwesome.Icon.faw_github),
+                SecondaryDrawerItem().withName(R.string.drawer_item_contact).withIcon(FontAwesome.Icon.faw_bullhorn)
+        )
     }
 
     private fun loadBackdrop() {
